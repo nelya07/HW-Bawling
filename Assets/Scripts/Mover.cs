ac00ca8c4bc1ad9224551d1dac12577b7af3c464
nelya07 <75816118+nelya07@users.noreply.github.com>
2020-12-16 02:05:39 +0300

    HW Bawling

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Mover : MonoBehaviour
+{
+ [SerializeField] private float _force;
+
+ private void Start()
+ {
+  GetComponent<Rigidbody>().AddForce(Vector3.forward * _force);
+ }
+}
+
+ 
